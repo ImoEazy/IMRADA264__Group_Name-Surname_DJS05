b10a001f0133bfb86324f717d81be06cd9274a9b
ImoEazy <imieadams@gmail.com>
2024-11-11 01:45:05 +0200

    added func tallreducer to handle state changes

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const initialSate = {
-    count : 0
-};
+import { createStore } from './store.js';
+import { tallyReducer } from './reducers.js';
+import { add, subtract, reset } from './actions.js';
 
-function reducer(state = initialSate, action) {
-    switch(action.type){
-        case 'ADD':
-            return{count: state.count + 1};
-        case 'SUBTRACT':
-            return{count: state.count - 1};
-        case 'RESET':
-            return{ count: 0};
-        default:
-            return state;            
-    }
-}
+// Creating store with the tallyReducer
+const store = createStore(tallyReducer);
+
+// Subscribing to state changes and logging the state
+store.subscribe(() => {
+  console.log('Current state:', store.getState());
+});
+
+// SCENARIO 1: Initial State Verification
+console.log('SCENARIO 1: Initial State Verification');
+console.log(store.getState()); 
+
+// SCENARIO 2: Incrementing the Counter
+console.log('SCENARIO 2: Incrementing the Counter');
+store.dispatch(add());
+store.dispatch(add());
+
+// SCENARIO 3: Decrementing the Counter
+console.log('SCENARIO 3: Decrementing the Counter');
+store.dispatch(subtract());
+
+// SCENARIO 4: Resetting the Counter
+console.log('SCENARIO 4: Resetting the Counter');
+store.dispatch(reset());
