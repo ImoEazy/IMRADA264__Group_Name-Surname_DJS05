1700c2c994d465ce06650792bac0a7a52281de99
ImoEazy <imieadams@gmail.com>
2024-11-10 23:14:24 +0200

    added reducer function

---
+++ script.js
@@ -0,0 +1,16 @@
+const initialSate = {
+    count : 0
+};
+
+function reducer(state = initialSate, action) {
+    switch(action.type){
+        case 'ADD':
+            return{count: state.count + 1};
+        case 'SUBTRACT':
+            return{count: state.count - 1};
+        case 'RESET':
+            return{ count: 0};
+        default:
+            return state;            
+    }
+}
