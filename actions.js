043e66279bb4b35f260d10346ccc815984bed7dd
ImoEazy <imieadams@gmail.com>
2024-11-12 12:57:46 +0200

    added notes

--- actions.js
+++ actions.js
@@ -1,4 +1,5 @@
 // actionTypes in redux context
+//consts used to describe what happened in app. Strings that will be dispatched as part of an action
 export const ADD = 'ADD';//This is the actual value of the constant, a string that will be used to identify the action in the system.
 export const SUBTRACT = 'SUBTRACT';
 export const RESET = 'RESET';
