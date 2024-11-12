d56bd3be1b9428bdef5566fc38e6d804ec2759bd
ImoEazy <imieadams@gmail.com>
2024-11-12 13:01:00 +0200

    added notes

--- actions.js
+++ actions.js
@@ -4,15 +4,18 @@ export const ADD = 'ADD';//This is the actual value of the constant, a string th
 export const SUBTRACT = 'SUBTRACT';
 export const RESET = 'RESET';
 
-// action creators
+// action creators; functions that will return action object
+//add(): This action creator returns an action with the type ADD, signaling that an "add" action should be processed.
 export function add() {
   return { type: ADD };
 }
 
+//subtract(): This action creator returns an action with the type SUBTRACT, signaling a "subtract" operation.
 export function subtract() {
   return { type: SUBTRACT };
 }
 
+//reset(): This action creator returns an action with the type RESET, signaling a reset operation.
 export function reset() {
   return { type: RESET };
 }
